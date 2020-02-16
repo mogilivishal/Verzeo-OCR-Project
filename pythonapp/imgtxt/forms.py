8bbd6941ec15a56dbc160537d351b00b9935c958
mogilivishal <38458250+mogilivishal@users.noreply.github.com>
2020-02-16 22:54:00 +0530

    Add files via upload

---
+++ pythonapp/imgtxt/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Document
+
+
+class DocumentForm(forms.Form):
+    docfile = forms.FileField(
+        label='Select a file'
+    )
+    class Meta:
+        model = Document
+        fields = ('docfile')
